bd455dfe25b5d410b5a18b7aef52a4e6bea40d61
Rob Myers <me.robmyers@gmail.com>
2025-03-04 09:16:00 +0000

    feat: add basic instructions

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-/** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null;
+  // 1. Close laptop or put device to sleep.
+  // 2. Open laptop or resume device.
+  // 3. Try editing text and observe no hot reload.
+  //
+  // Happens in Chrome Version 133.0.6943.142 (Official Build) (arm64).
+  // Does not happen in Firefox 135.0.1 (aarch64).
+  return <div>Hello, world!!</div>;
 }
